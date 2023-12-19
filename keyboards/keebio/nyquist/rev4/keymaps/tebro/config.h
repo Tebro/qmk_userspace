dd56b177567d8bb43bf2b6704e0ad264dc2d613b
Richard Weber <riche.weber@gmail.com>
2023-12-19 19:00:50 +0200

    RGB evaluation

---
+++ keyboards/keebio/nyquist/rev4/keymaps/tebro/config.h
@@ -0,0 +1,5 @@
+// The pin connected to the data pin of the LEDs
+//#define WS2812_DI_PIN D7
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
+
--- keyboards/keebio/nyquist/rev4/keymaps/tebro/keymap.c
+++ keyboards/keebio/nyquist/rev4/keymaps/tebro/keymap.c
@@ -33,7 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-
-
-
-
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    for (uint8_t i = led_min; i < led_max; i++) {
+        switch(get_highest_layer(layer_state|default_layer_state)) {
+            case 3:
+                rgb_matrix_set_color(i, RGB_AZURE);
+                break;
+            case 2:
+                rgb_matrix_set_color(i, RGB_BLUE);
+                break;
+            case 1:
+                rgb_matrix_set_color(i, RGB_RED);
+                break;
+            default:
+                break;
+        }
+    }
+    return false;
+}
