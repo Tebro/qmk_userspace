f8e0e7548843b611c3a7eb9bc82b9e015ffa0a6a
Richard Weber <riche.weber@gmail.com>
2023-12-19 17:40:51 +0200

    Fix keycodes vs via variants

--- keyboards/keebio/nyquist/rev4/keymaps/tebro/keymap.c
+++ keyboards/keebio/nyquist/rev4/keymaps/tebro/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,      KC_W,     KC_E,        KC_R,        KC_T,        KC_Y,     KC_U,        KC_I,     KC_O,     KC_P,     KC_LBRC,
         KC_BSPC,  KC_A,      KC_S,     KC_D,        KC_F,        KC_G,        KC_H,     KC_J,        KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,      KC_X,     KC_C,        KC_V,        KC_B,        KC_N,     KC_M,        KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
-        KC_LCTL,  KC_LCTL,   KC_LALT,  FN_MO13,     KC_LGUI,     KC_SPC,      KC_ENT,   FN_MO23,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RCTL
+        KC_LCTL,  MO(3),     KC_LALT,  MO(1),       KC_LGUI,     KC_SPC,      KC_ENT,   MO(2),       KC_LEFT,  KC_DOWN,  KC_UP,    KC_RCTL
     ),
     [1] = LAYOUT_ortho_5x12(
         RGB_TOG,  RGB_M_P,   RGB_M_B,  RGB_M_R,     RGB_M_SW,    KC_NO,       KC_NO,    KC_NO,       KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT_ortho_5x12(
         _______,  KC_F1,     KC_F2,    KC_F3,       KC_F4,       KC_F5,       KC_F6,    KC_F7,       KC_F8,    KC_F9,    KC_F10,   KC_F11,
-        _______,  _______,   S(KC_2),  S(KC_LBRC),  S(KC_RBRC),  S(KC_BSLS),  KC_NLCK,  KC_P7,       KC_P8,    KC_P9,    S(KC_8),  KC_F12,
+        _______,  _______,   S(KC_2),  S(KC_LBRC),  S(KC_RBRC),  S(KC_BSLS),  KC_NUM,   KC_P7,       KC_P8,    KC_P9,    S(KC_8),  KC_F12,
         KC_DEL,   KC_MINS,   S(KC_4),  S(KC_9),     S(KC_0),     KC_GRV,      _______,  KC_P4,       KC_P5,    KC_P6,    _______,  _______,
         _______,  KC_EQL,    S(KC_6),  KC_LBRC,     KC_RBRC,     S(KC_GRV),   S(KC_7),  KC_P1,       KC_P2,    KC_P3,    KC_BSLS,  _______,
         _______,  _______,   _______,  _______,     _______,     _______,     _______,  _______,     KC_P0,    KC_PDOT,  _______,  _______
